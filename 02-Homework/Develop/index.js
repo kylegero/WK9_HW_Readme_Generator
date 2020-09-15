f05a501194186d0eb59b2d6ef699c4f487dd7b28
Kyle Pykonen <jjrokkos@gmail.com>
2020-09-14 21:39:09 -0700

    Add files via upload
    Added array of questions for user, added const inquirer

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -1,7 +1,51 @@
+const inquirer = require("inquirer");
 // array of questions for user
-const questions = [
-
-];
+const questions = [{
+    type: "input",
+    message: "What is your project's title?",
+    name: "title"
+},
+{
+    type: "input",
+    message: "How would you describe your project?",
+    name: "Description"
+},
+{
+    type: "input",
+    message: "If you have the time, please provide some installation instructions for your project.",
+    name: "Installation"
+},
+{
+    type: "input",
+    message: "Help us fill out the Usage section with some instructions and examples of your project in action.",
+    name: "usage"
+},
+{
+    type: "input",
+    message: "Please give us some guidelines for future contributions to your project",
+    name: "contribution"
+},
+{
+    type: "input",
+    message: "Tell us how we can test your application please.",
+    name: "tests"
+},
+{
+    type: "list",
+    message: "What license does your project require?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+    name: "license"
+},
+{
+    type: "input",
+    message: "Please enter your Github username",
+    name: "username"
+},
+{
+    type: "input",
+    message: "Lastly, please enter your email address.",
+    name: "email"
+}];
 
 // function to write README file
 function writeToFile(fileName, data) {
